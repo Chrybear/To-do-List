a057e3f67ea0397720e5ab7631d738f21068c510
Ryan Barrett <>
2023-08-15 16:13:06 -0400

    Functional improvements and made less ugly

--- to-do.py
+++ to-do.py
@@ -2,16 +2,16 @@
 # Author: Ryan Barrett
 # Date: 8/8/2023
 # Desc: Simple To-do list application for programming practice
+# Imports:
 import tkinter
-# Imports
 from os.path import exists
 import pickle
 import customtkinter as tk
 
-# TODO make it not ugly
-
 
 # Object for a list entry
+# Current implementation would work with just a hash table and does not need a custom object.
+# However, using an object opens possibilities for adding additional functionality, such as task priority, for a task.
 class Entry:
     def __init__(self, val="", completed=False):
         self.val = val
@@ -31,6 +31,7 @@ def save_list(lis: list):
     pickle.dump(lis, open('to-do.pkl', 'wb'))  # Will create new file if one does not currently exist
 
 
+# Function to print out all tasks currently saved into the console.
 def print_list(lis):
     for key, value in lis.items():
         print(key, value.val)
@@ -39,27 +40,44 @@ def print_list(lis):
 if __name__ == '__main__':
     # On startup, load list if one already exists, else initialize an empty list
     cur_list = get_list()
-    print_list(cur_list)
+    # for i in range(100):
+    #     cur_list[str(i)] = Entry(val=str(i))
+    # print_list(cur_list)
 
-    # Build GUI TODO move this into own function
-    tk.set_appearance_mode("System")
+    # Build GUI
+    tk.set_appearance_mode("dark")
     tk.set_default_color_theme("blue")
     root = tk.CTk()
     root.title("To-do List")
-    root.geometry("700x450")
+    # root.geometry("900x750")
     frame = tk.CTkFrame(master=root)
-    frame.pack(pady=20, padx=60, fill="both", expand=True)
+    frame.pack(pady=10, padx=10, fill="both", expand=True)
 
-    task_frame = tk.CTkFrame(master=frame)
-    task_frame.pack()
+    task_frame = tk.CTkFrame(master=frame, fg_color="transparent")
+    task_frame.pack(anchor="center", expand=True)
+
+    task_box_frame = tk.CTkFrame(master=task_frame, fg_color="#ffccff", bg_color="#ffccff")
+    task_box_frame.pack(side="left", expand=True, ipadx=5, ipady=5)
     # List of our current tasks
-    task_box = tkinter.Listbox(master=task_frame,
-                               width=25,
-                               height=5)
-    task_box.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
-    # Add a scroll-bar to it
-    scroll_bar = tk.CTkScrollbar(master=task_frame)
-    scroll_bar.pack(side=tkinter.RIGHT)
+    task_box = tkinter.Listbox(master=task_box_frame,
+                               width=40,
+                               font=("sans-serif", 20, "bold"),
+                               bg="#ffccff",
+                               bd=0,
+                               highlightthickness=0,
+                               activestyle="none",
+                               )
+    task_box.pack(side="left", expand=True)
+    # Add scroll-bars to the task box
+    y_scroll_bar = tk.CTkScrollbar(master=task_box_frame, orientation="vertical")
+    # x_scroll_bar = tk.CTkScrollbar(master=task_box_frame, orientation="horizontal")
+    task_box.configure(yscrollcommand=y_scroll_bar.set)
+    # task_box.configure(xscrollcommand=x_scroll_bar.set)
+
+    y_scroll_bar.configure(command=task_box.yview)
+    # x_scroll_bar.configure(command=task_box.xview)
+    y_scroll_bar.pack(side="right", fill='y')
+    # x_scroll_bar.pack(side="bottom", fill='x')
 
     # Function to empty and draw the task list
     def draw_tasks(show_complete=False):
@@ -73,16 +91,22 @@ if __name__ == '__main__':
     draw_tasks()
 
     # Task label and entry
-    task_label = tk.CTkLabel(master=frame, text="Enter New Task")
-    task_label.pack(padx=10)
+    entry_frame = tk.CTkFrame(master=frame)
+    entry_frame.pack(pady=10, padx=10, anchor="s")
 
-    task_entry = tk.CTkEntry(master=frame, placeholder_text="I wanna...")
-    task_entry.pack(pady=5, padx=10)
+    task_label = tk.CTkLabel(master=entry_frame, text="Enter New Task")
+    task_label.pack(padx=10, side=tkinter.LEFT)
+
+    task_entry = tk.CTkEntry(master=entry_frame, placeholder_text="I wanna...")
+    task_entry.pack(pady=5, padx=10, side=tkinter.LEFT)
 
     # Function to add a new task to the current task list
     def add_task():
-        task = task_entry.get()
+        task = task_entry.get().strip()  # We want to remove redundant spaces/blank entries
         if task and task not in cur_list:
+            # Need to account for rabble-rousers that may enter very long tasks
+            # We will cut tasks down to 50 chars at most
+            task = task[:51]
             cur_list[task] = (Entry(val=task))
             save_list(cur_list)
             # Update the list of tasks
@@ -104,12 +128,12 @@ if __name__ == '__main__':
 
     # Function to swap to viewing completed/un-completed tasks
     def swap_view():
-        show_complete = False if "Show Completed Tasks" == change_view_button.cget('text') else True
+        show_complete = False if "Show Completed" == change_view_button.cget('text') else True
         draw_tasks(not show_complete)
         if show_complete:
-            change_view_button.configure(text="Show Completed Tasks")
+            change_view_button.configure(text="Show Completed")
         else:
-            change_view_button.configure(text="Show Un-completed Tasks")
+            change_view_button.configure(text="Show Un-completed")
 
     # Function to delete a task
     def delete_task():
@@ -123,7 +147,7 @@ if __name__ == '__main__':
                 task_box.delete(cur_task[0], cur_task[0])
                 # Update tasks shown
                 # makes sure we're showing the current view
-                draw_tasks("Show Un-completed Tasks" == change_view_button.cget('text'))
+                draw_tasks("Show Un-completed" == change_view_button.cget('text'))
                 # Save tasks
                 save_list(cur_list)
 
@@ -136,27 +160,39 @@ if __name__ == '__main__':
         # Save list
         save_list(cur_list)
         # Update view
-        draw_tasks("Show Un-completed Tasks" == change_view_button.cget('text'))
+        draw_tasks("Show Un-completed" == change_view_button.cget('text'))
 
-    # Add button
-    add_button = tk.CTkButton(master=frame, text="Add", command=add_task)
-    add_button.pack(pady=5, padx=10)
+    # Button frame
+    button_frame = tk.CTkFrame(master=task_frame,
+                               bg_color="transparent",
+                               fg_color="transparent",
+                               width=30,
+                               height=10
+                               )
+    button_frame.pack(side='right')
+    # Add button (added to entry frame as this button relates to entering a task)
+    add_button = tk.CTkButton(master=entry_frame, text="Add", command=add_task, fg_color="#9900cc")
+    add_button.pack(pady=10, padx=10, side=tkinter.LEFT)
 
     # Complete button
-    mark_done_button = tk.CTkButton(master=frame, text="Mark Completed", command=complete_task)
-    mark_done_button.pack(pady=5, padx=10)
+    mark_done_button = tk.CTkButton(master=button_frame, text="Mark Completed", command=complete_task, 
+                                    fg_color="#9900cc")
+    mark_done_button.pack(pady=15, padx=10, fill="both")
 
     # Delete task
-    delete_task_button = tk.CTkButton(master=frame, text="Delete Task", command=delete_task)
-    delete_task_button.pack(pady=5, padx=10)
+    delete_task_button = tk.CTkButton(master=button_frame, text="Delete Task", command=delete_task,
+                                      fg_color="#9900cc")
+    delete_task_button.pack(pady=15, padx=10, fill="both")
 
     # Show completed/show un-completed button
-    change_view_button = tk.CTkButton(master=frame, text="Show Completed Tasks", command=swap_view)
-    change_view_button.pack(pady=5, padx=10)
+    change_view_button = tk.CTkButton(master=button_frame, text="Show Completed", command=swap_view,
+                                      fg_color="#9900cc")
+    change_view_button.pack(pady=15, padx=10, fill="both")
 
     # Clear completed tasks
-    clear_completed_button = tk.CTkButton(master=frame, text="Clear Completed Tasks", command=clear_completed)
-    clear_completed_button.pack(pady=5, padx=10)
+    clear_completed_button = tk.CTkButton(master=button_frame, text="Clear Completed Tasks", command=clear_completed,
+                                          fg_color="#9900cc")
+    clear_completed_button.pack(pady=15, padx=10, fill="both")
 
     # Start GUI
     root.mainloop()
